92ad9f7d2c5916db3315071bae6332d6999ff96a
Louise Morais <louisemorais3@gmail.com>
2025-03-12 00:51:56 -0300

    feat: adição dos Setter e Getters dos atributos

--- src/models/Localizacao.java
+++ src/models/Localizacao.java
@@ -6,4 +6,35 @@ public class Localizacao {
     private double latitude;
     private double longitude;
     private int idAreaVerde;
+
+    //set
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setLatitude(double latitude){
+        this.latitude=latitude;
+    }
+    public void setLongitude(double longitude){
+        this.longitude=longitude;
+    }
+    public void setIdAreaVerde(int idAreaVerde){
+        this.idAreaVerde= idAreaVerde;
+    }
+
+    //get
+    public int getId(){
+        return id;
+    }
+    public double getLatitude(){
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public int getIdAreaVerde() {
+        return idAreaVerde;
+    }
+
 }
