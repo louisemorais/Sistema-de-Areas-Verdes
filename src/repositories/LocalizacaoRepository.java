0c3066b825c4e4066634cfc6d92b4fbf2fb627f6
Louise Morais <louisemorais3@gmail.com>
2025-03-12 13:28:47 -0300

    feat: criado o método salvarDados da classe Localização

--- src/repositories/LocalizacaoRepository.java
+++ src/repositories/LocalizacaoRepository.java
@@ -7,4 +7,9 @@ import java.util.List;
 public class LocalizacaoRepository {
     private static List<Localizacao> LOCALIZACAO = new ArrayList<>();
     private static int id= 1;
+
+    public void salvarDados(Localizacao dados){
+        dados.setId(id++);
+        LOCALIZACAO.add(dados);
+    }
 }
