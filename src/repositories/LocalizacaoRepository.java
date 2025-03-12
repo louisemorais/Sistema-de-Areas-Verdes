e2ccc27ac7a0d157ca38797c608c9692f8fa3ea5
Louise Morais <louisemorais3@gmail.com>
2025-03-12 13:22:15 -0300

    feat: criação do Repository de localização e seus atributos estáticos

---
+++ src/repositories/LocalizacaoRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import models.Localizacao;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocalizacaoRepository {
+    private static List<Localizacao> LOCALIZACAO = new ArrayList<>();
+    private static int id= 1;
+}
