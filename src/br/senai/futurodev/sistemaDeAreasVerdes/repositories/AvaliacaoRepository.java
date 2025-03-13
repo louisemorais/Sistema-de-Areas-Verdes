a73c54d6a30f92247502a36e9976904a29617127
Louise Morais <louisemorais3@gmail.com>
2025-03-13 16:28:58 -0300

    feat: Criação da classe Avaliação Repository e seus atributos estáticos

---
+++ src/br/senai/futurodev/sistemaDeAreasVerdes/repositories/AvaliacaoRepository.java
@@ -0,0 +1,10 @@
+package br.senai.futurodev.sistemaDeAreasVerdes.repositories;
+
+import br.senai.futurodev.sistemaDeAreasVerdes.models.Avaliacao;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AvaliacaoRepository {
+    public static List<Avaliacao> AVALIACOES = new ArrayList<>();
+    private static int id= 1;
+}
