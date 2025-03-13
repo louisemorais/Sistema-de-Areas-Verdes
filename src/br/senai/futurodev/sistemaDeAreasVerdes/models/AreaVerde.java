e44038bf2c76947ee27474dbc1e19f9b5d6b4c8d
Louise Morais <louisemorais3@gmail.com>
2025-03-13 19:04:22 -0300

    feat: adição dos Setter e mudança de variável

--- src/br/senai/futurodev/sistemaDeAreasVerdes/models/AreaVerde.java
+++ src/br/senai/futurodev/sistemaDeAreasVerdes/models/AreaVerde.java
@@ -6,8 +6,31 @@ public class AreaVerde {
     private int id;
     private String nome;
     private String tipoDeVegetacao;
-    private String horariosDeFuncionamento;
+    private double horariosDeFuncionamento;
     private Localizacao localizacao;
     private List<String> atividadesDisponiveis;
     private List<Avaliacao> listaDeAvaliacoes;
+
+    //set
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public void setTipoDeVegetacao(String tipoDeVegetacao){
+        this.tipoDeVegetacao=tipoDeVegetacao;
+    }
+    public void setHorariosDeFuncionamento(double horariosDeFuncionamento){
+        this.horariosDeFuncionamento=horariosDeFuncionamento;
+    }
+    public void setLocalizacao(Localizacao localizacao){
+        this.localizacao=localizacao;
+    }
+    public void setAtividadesDisponiveis(List atividadesDisponiveis){
+        this.atividadesDisponiveis=atividadesDisponiveis;
+    }
+    public void setListaDeAvaliacoes(List avaliacoes){
+        this.listaDeAvaliacoes=avaliacoes;
+    }
 }
