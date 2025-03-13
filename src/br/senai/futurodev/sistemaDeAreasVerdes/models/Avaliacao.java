8dffeba47cd5009c6e1b3b22c69bee36d638b1bd
Louise Morais <louisemorais3@gmail.com>
2025-03-13 14:52:21 -0300

    feat: criado a classe Avaliação e seus atributos

---
+++ src/br/senai/futurodev/sistemaDeAreasVerdes/models/Avaliacao.java
@@ -0,0 +1,11 @@
+package br.senai.futurodev.sistemaDeAreasVerdes.models;
+
+public class Avaliacao {
+    public int id;
+    public double qualidadeArvore;
+    public double qualidadeAr;
+    public double poluicaosonora;
+    public double coletaDeResiduo;
+    public double transportePublico;
+    public String tipoDeAreaVerde;
+}
