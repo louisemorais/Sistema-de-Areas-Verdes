4675a578c775c708ed1cdaabff99defcba72e5d3
Louise Morais <louisemorais3@gmail.com>
2025-03-13 14:54:34 -0300

    feat: criado os Setter dos atributos

--- src/br/senai/futurodev/sistemaDeAreasVerdes/models/Avaliacao.java
+++ src/br/senai/futurodev/sistemaDeAreasVerdes/models/Avaliacao.java
@@ -8,4 +8,33 @@ public class Avaliacao {
     public double coletaDeResiduo;
     public double transportePublico;
     public String tipoDeAreaVerde;
+
+    //set
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public void setQualidadeArvore(double qualidadeArvore){
+        this.qualidadeArvore=qualidadeArvore;
+    }
+
+    public void setQualidadeAr(double qualidadeAr) {
+        this.qualidadeAr = qualidadeAr;
+    }
+
+    public void setAusenciaPoluicaoSonora(double ausenciapoluicaosonora) {
+        this.poluicaosonora=ausenciapoluicaosonora;
+    }
+
+    public void setColetaDeResiduo(double coletaDeResiduo) {
+        this.coletaDeResiduo= coletaDeResiduo;
+    }
+
+    public void setTransportePublico(double transportePublico) {
+        this.transportePublico = transportePublico;
+    }
+
+    public void setipoDeAreaVerde(String tipoDeAreaVerde) {
+        this.tipoDeAreaVerde = tipoDeAreaVerde;
+    }
 }
