b517f4ad87702c4f231715294319eaae567b0063
Louise Morais <louisemorais3@gmail.com>
2025-03-15 15:07:23 -0300

    feat: implementação do algoritmo de listagem de todas as áreas verdes

--- src/br/senai/futurodev/sistemaDeAreasVerdes/main/Main.java
+++ src/br/senai/futurodev/sistemaDeAreasVerdes/main/Main.java
@@ -1,5 +1,8 @@
 package br.senai.futurodev.sistemaDeAreasVerdes.main;
 
+import br.senai.futurodev.sistemaDeAreasVerdes.repositories.AreaVerdeRepository;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +14,11 @@ public class Main {
             System.out.printf("1- Listar Áreas Verdes\n" + "2- Avaliar Área Verde\n" + "3- Ver detalhe de uma Área Verde\n" +
                     "4- Cadastrar nova Área Verde\n" + "0- Sair" +"\n"+"Digite a opção que deseja acessar:");
             int escolha = input.nextInt();
+
             switch (escolha) {
                 case 1:
+                    List todos= AreaVerdeRepository.listarTodos();
+                    System.out.println(todos);
                     break;
                 case 2:
                     break;
