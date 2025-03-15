03cbceac586f1e5137721301c4e9ab959e024503
Louise Morais <louisemorais3@gmail.com>
2025-03-14 22:07:08 -0300

    feat: criação da base do menu

--- src/br/senai/futurodev/sistemaDeAreasVerdes/main/Main.java
+++ src/br/senai/futurodev/sistemaDeAreasVerdes/main/Main.java
@@ -1,7 +1,29 @@
 package br.senai.futurodev.sistemaDeAreasVerdes.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.printf("1- Listar Áreas Verdes\n" + "2- Avaliar Área Verde\n" + "3- Ver detalhe de uma Área Verde\n" +
+                "4- Cadastrar nova Área Verde\n" + "0- Sair" +"\n"+"Digite a opção que deseja acessar:");
+        int escolha = input.nextInt();
+        Boolean rodando=true;
+
+        while (rodando){
+            switch (escolha) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    rodando=false;
+                    break;
+            }
+        }
     }
 }
