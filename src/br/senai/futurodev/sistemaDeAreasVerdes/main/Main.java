e3bcc66ddd514cc34c2b81c111560b7fbff28008
Louise Morais <louisemorais3@gmail.com>
2025-03-15 15:03:45 -0300

    fix: correção do bug de looping infinito

--- src/br/senai/futurodev/sistemaDeAreasVerdes/main/Main.java
+++ src/br/senai/futurodev/sistemaDeAreasVerdes/main/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.printf("1- Listar Áreas Verdes\n" + "2- Avaliar Área Verde\n" + "3- Ver detalhe de uma Área Verde\n" +
-                "4- Cadastrar nova Área Verde\n" + "0- Sair" +"\n"+"Digite a opção que deseja acessar:");
-        int escolha = input.nextInt();
         Boolean rodando=true;
 
         while (rodando){
+            System.out.printf("1- Listar Áreas Verdes\n" + "2- Avaliar Área Verde\n" + "3- Ver detalhe de uma Área Verde\n" +
+                    "4- Cadastrar nova Área Verde\n" + "0- Sair" +"\n"+"Digite a opção que deseja acessar:");
+            int escolha = input.nextInt();
             switch (escolha) {
                 case 1:
                     break;
